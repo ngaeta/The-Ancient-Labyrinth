1d628788a767d9b93aa9b60753c56fe2a2344a32
ngaeta <40716938+ngaeta@users.noreply.github.com>
2018-07-01 17:30:56 +0200

    Add files via upload

---
+++ Scripts/UI/UIElementPointerEnter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIElementPointerEnter : MonoBehaviour {
+
+	public Color color;
+
+	private Image image;
+	private Color defaultColor;
+
+	void Start() {
+		
+		image = GetComponent<Image> ();
+		defaultColor = image.color;
+	}
+
+	public void ColorPointerEnter() {
+		image.color = color;
+	}
+
+	public void ColorPointerExit() {
+		image.color = defaultColor;
+	}
+
+	void OnDisable() {
+		ColorPointerExit ();
+	}
+}
